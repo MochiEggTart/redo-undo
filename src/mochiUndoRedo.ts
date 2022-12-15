189bd92c450924b189806bd0edc4fd8a34ad4627
CANVAS <peter833112001@gmail.com>
2022-12-15 13:44:24 +0800

    feat: add undo、redo func

---
+++ src/mochiUndoRedo.ts
@@ -0,0 +1,101 @@
+interface Present {
+    name: string,
+    value: any,
+    customData?: { [key: string]: any }
+}
+
+class MochiUndoRedo {
+
+    //紀錄(上一步)
+    private past: Present[] = [];
+
+    //目前紀錄
+    private present: Present = {
+        name: "",
+        value: null,
+        customData: {}
+    };
+
+    //紀錄(下一步)
+    private future: Present[] = [];
+
+    //取得步驟數量
+    get count() {
+        return this.past.length;
+    }
+
+    //取得目前步驟資料
+    getData(index?: number) {
+
+        if (index) {
+
+            return [...this.past, this.present, ...this.future][index];
+
+        }
+
+        return this.present;
+
+    }
+
+    //取得整個步驟陣列
+    getDataList() {
+        return [...this.past, this.present, ...this.future];
+    }
+
+    //寫入紀錄
+    add(data: Present) {
+        this.past.push(this.present);
+        this.present = data;
+        this.future = [];
+    }
+
+    //上一步
+    undo(count: number = 1) {
+
+        if (count < 1) {
+            return;
+        }
+
+        const { past, future, present } = this;
+
+        if (past.length > 0) {
+            const pos = past.length - count;
+            const previous = past[pos];
+            const newthis = past.slice(0, pos);
+            this.past = newthis;
+            this.future = [...past.slice(pos + 1, past.length), present, ...future];
+            this.present = previous;
+        }
+    }
+
+    //下一步
+    redo(count: number = 1) {
+
+        if (count < 1) {
+            return;
+        }
+
+        const { past, future, present } = this;
+
+        if (this.future.length > 0) {
+            const next = future[count - 1];
+            const newthis = future.slice(count);
+            this.past = [...past, present, ...future.slice(0, count - 1)];
+            this.present = next;
+            this.future = newthis;
+        }
+    }
+
+    //清空所有紀錄
+    clearAll() {
+        this.past = [];
+        this.present = {
+            name: "",
+            value: null,
+            customData: {}
+        };
+        this.future = [];
+    }
+}
+
+export { MochiUndoRedo };
